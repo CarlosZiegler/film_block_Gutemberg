65f250369d6d5cc241d2d21a9bd6b2f5d09be3ce
CarlosZiegler <carlos.ziegler@gmail.com>
2019-04-10 16:32:56 +0200

    styles with Bootstrap

--- Film-plugin/film-plugin.php
+++ Film-plugin/film-plugin.php
@@ -23,16 +23,16 @@
     function films_attach_theme_options()
     {
         // original do tutorial
-         // register Stylesheet of Plugin
+        // register Stylesheet of Plugin
         wp_register_script(
             'custom_admin_js',
             //where is my style file
-            plugin_dir_url( __FILE__ ) . 'assets/js/bootstrap.js'
-            
+            plugin_dir_url(__FILE__) . 'assets/js/bootstrap.js'
+        
         );
         //enqueue Style in $WP_filter
-        wp_enqueue_style( 'custom_admin_js' );
-    
+        wp_enqueue_style('custom_admin_js');
+        
         wp_register_style(
             'custom_admin_css',
             //where is my style file
@@ -48,14 +48,18 @@
         
         ->add_fields(array( // type , slug , Titel of field
                          Field::make('text', 'name_film', 'Name of the Film'),
-                         Field::make('text', 'director', 'Director'),
                          Field::make('image', 'poster', 'Poster'),
+                         Field::make('text', 'director', 'Director'),
+                         Field::make( 'textarea', 'description_film', __( 'Description' ) )
+                             ->set_rows( 4 ),
                          Field::make('text', 'wikipedia', 'Link Wikipedia'),
                          Field::make('text', 'trailer', 'Link trailer'),
+                         
                          // Complex fields from Carbon Fields with Class Fiel from Carbon Fields
                          Field::make('complex', 'cast', 'Movies cast')
                              ->add_fields(array(
                                               Field::make('text', 'actor', 'Actor'),
+                                              Field::make('image', 'actor_foto', 'Foto Actor')
                                           )),
                      ))
             // Set a Style
@@ -69,52 +73,69 @@
             // Function renderize Block, musst return a String HTML
             ->set_render_callback(function ($block) {
                 
-                
                 // The HTML Template
                 ?>
                 
-                
                 <div class="container card">
+                
+                <?php ?>
+                
+                <div class="card-body">
+                <?php if (esc_html($block['name_film']) !== '') { ?>
+                    <h1 class="text-center card-title"><strong><?php echo esc_html($block['name_film']); ?></strong>
+                    </h1>
+                <?php } ?>
+                
+                <?php if (esc_html($block['poster']) !== '') { ?>
+                    <div class="container"><?php echo wp_get_attachment_image($block['poster'], array('700', '600'), "", array("class" => "rounded mx-auto d-block card-img-top ")); ?></div>
+                <?php } ?>
                     
+                    <h3 class="card-text"><strong>Director: </strong>  <?php echo esc_html($block['director']); ?></strong></h3>
                     
-                    <div class="card-body">
-                        <h1 class="text-center card-title"><strong><?php echo esc_html($block['name_film']); ?></strong></h1>
-                        <?php echo wp_get_attachment_image($block['poster'], array('700', '600'), "", array("class" => "rounded mx-auto d-block card-img-top ")); ?>
-                        
-                        <h3 class="card-text">Director: <strong> <?php echo esc_html($block['director']); ?></strong></h3>
-                        
-                        
-                        <ul class="card-text list-group"><h3> Cast:</h3>
-                            <?php
-                                // We need to use a foreach to read the complex field elements
-                                foreach ($block['cast'] as $actor) {
-                                    ?>
-                                    <li class="list-group-item list-group-item-info">
-                                        <?php echo esc_html($actor['actor']); ?>
-                                    </li>
-                                <?php }
+                    <h3 class="card-text"><strong>Description:</strong> </h3><p class="text-justify" > <?php echo esc_html($block['description_film']); ?></p>
+                    
+                    <h3> <strong>Cast:</strong></h3>
+                    <?php
+                        // We need to use a foreach to read the complex field elements
+                        foreach ($block['cast'] as $actor) {
                             ?>
-                        </ul>
-                        <div class="embed-responsive embed-responsive-16by9 mt-2">
-                            <?php $link_video = $block['trailer'];
-                                $changed = "watch?v=";
-                                $link_video = str_replace($changed, "embed/", $link_video);
-                            ?>
+    
+                            <figure class="figure p-1">
+                                <?php echo wp_get_attachment_image(esc_html($actor['actor_foto']), array('150', ''), "", array("class" => "figure-img img-fluid rounded")); ?>
+                                <figcaption class="figure-caption text-center">  <?php echo esc_html($actor['actor']); ?></figcaption>
+                            </figure>
                             
-                            <iframe class="embed-responsive-item" src="<?php echo $link_video; ?>" width="640" height="360" frameborder="0"></iframe>
-                        </div>
-                        <div class="container margin">
-                        <a href="<?php echo $block['wikipedia'] ?>" target="_blank" class="btn btn-primary mt-2">zum Wikipedia</a>
-                        </div>
+                                   
+                            
+                
+                        <?php }
+                    ?>
+    
+    
+    
+    
+                    <h3> <strong>Trailer:</strong></h3>
+                    <div class="embed-responsive embed-responsive-16by9 mt-2">
+                        <?php $link_video = esc_html($block['trailer']);
+                            $changed = "watch?v=";
+                            $link_video = str_replace($changed, "embed/", $link_video);
+                        ?>
+                        
+                        <iframe class="embed-responsive-item" src="<?php echo $link_video; ?>" width="640" height="360"
+                                frameborder="0"></iframe>
                     </div>
-                </div>
-                
-                
-                <?php
-                
-            }
+                    <div class="container margin">
+                        <a href="<?php echo $block['wikipedia'] ?>" target="_blank" class="btn btn-info mt-2">zum Wikipedia</a>
+                    </div>
+                    </div>
+                    </div>
+                    
+                    
+                    <?php
+                    
+                }
             );
-    }
+            }
     
     
     add_action('carbon_fields_register_fields', 'films_attach_theme_options');
